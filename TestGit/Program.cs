1cf6ce7c18def33acbf43377a1c81ebc617bb59c
Magno Mateus Almeida de Oliveira <magnomao@gmail.com>
2020-12-10 21:14:45 -0300

    Added the message 'Another string'

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -4,6 +4,7 @@ namespace TestGit {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Another string!");
         }
     }
 }
